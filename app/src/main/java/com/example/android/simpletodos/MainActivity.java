f43fe5b23c99d3c2cf8d40aa2a99dff82ec3d572
Rob Hess <robwhess@gmail.com>
2018-01-18 14:31:04 -0800

    Add code from 1/18 lecture.

--- app/src/main/java/com/example/android/simpletodos/MainActivity.java
+++ app/src/main/java/com/example/android/simpletodos/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.android.simpletodos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayDeque;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTodoListTextView;
+    private EditText mTodoEntryEditText;
+
+    private ArrayDeque<String> mTodoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView todoListTextView = (TextView)findViewById(R.id.tv_todo_list);
-        todoListTextView.setText("Finish the TODO app");
+        mTodoList = new ArrayDeque<String>();
+        mTodoListTextView = (TextView)findViewById(R.id.tv_todo_list);
+        mTodoEntryEditText = (EditText)findViewById(R.id.et_todo_entry);
+
+        Button addTodoButton = (Button)findViewById(R.id.btn_add_todo);
+        addTodoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoText = mTodoEntryEditText.getText().toString();
+                if (!TextUtils.isEmpty(todoText)) {
+                    mTodoList.push(todoText);
+                    mTodoListTextView.setText("");
+                    for (String todo : mTodoList) {
+                        mTodoListTextView.append(todo + "\n\n");
+                    }
+                    mTodoEntryEditText.setText("");
+                }
+            }
+        });
+
     }
 }
