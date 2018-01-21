d97b0845af406902f0140327aaee1585e8b01860
wongnich <wongnich@gmail.com>
2018-01-20 16:48:57 -0800

    Trying to add RecyclerView to the Todo List, app crashes

--- app/src/main/java/com/example/android/simpletodos/MainActivity.java
+++ app/src/main/java/com/example/android/simpletodos/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.simpletodos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTodoList = new ArrayDeque<String>();
-        mTodoListTextView = (TextView)findViewById(R.id.tv_todo_list);
+        mTodoListTextView = (TextView)findViewById(R.id.tv_todo_text);
         mTodoEntryEditText = (EditText)findViewById(R.id.et_todo_entry);
 
         Button addTodoButton = (Button)findViewById(R.id.btn_add_todo);
---
+++ app/src/main/java/com/example/android/simpletodos/TodoAdapter.java
@@ -0,0 +1,73 @@
+package com.example.android.simpletodos;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wongnich on 1/20/18.
+ */
+
+public class TodoAdapter
+        extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder> {
+
+    private ArrayList<String> mTodoList;
+
+    public TodoAdapter() {
+        mTodoList = new ArrayList<String>();
+    }
+
+
+    public void addTodo(String todo) {
+        mTodoList.add(todo);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTodoList.size();
+    }
+
+    @Override
+    public TodoViewHolder onCreateViewHolder(ViewGroup viewGroup,
+                                             int viewType) {
+        LayoutInflater inflater =
+                LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.todo_list_item, viewGroup,
+                false);
+        TodoViewHolder viewHolder = new TodoViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(TodoViewHolder holder,
+                                 int position) {
+        String todo =
+                mTodoList.get(mTodoList.size() - position - 1);
+        holder.bind(todo);
+    }
+
+
+    /*Our Todo View Holder class*/
+    class TodoViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTodoTextView;
+
+        public TodoViewHolder(View itemView) {
+            super(itemView);
+            mTodoTextView =
+                    (TextView)itemView.findViewById(R.id.tv_todo_text);
+        }
+
+        void bind(String todo) {
+            mTodoTextView.setText(todo);
+        }
+
+
+    }
+
+
+}
